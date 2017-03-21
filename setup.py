24a72790ff6c18ada5e18090f303c0c723c54ca7
Raphaël Bleuse <raphael.bleuse@imag.fr>
2017-03-21 18:27:54 +0100

    Package module

---
+++ setup.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+# All packaging information is located in the setup.cfg file.
+setup(
+    py_modules=['procset'],  # should be in setup.cfg, but it is not supported
+)
--- tests/procset.py
+++
@@ -1,1 +0,0 @@
-../procset/procset.py
