54e9672f232a5a35b5c07142283f17879babae8b
Raphaël Bleuse <raphael.bleuse@imag.fr>
2017-03-22 10:00:07 +0100

    Fix packaging of deprecated API

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ from setuptools import setup
 
 # All packaging information is located in the setup.cfg file.
 setup(
-    py_modules=['procset'],  # should be in setup.cfg, but it is not supported
+    py_modules=[  # should be in setup.cfg, but it is not supported
+        'procset',
+        'intsetwrap',  # transition module, planned for removal
+    ],
 )
